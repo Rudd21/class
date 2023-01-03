101245bdc2a2a705c3a1dd4ba814aab905612407
Rudd21 <94556419+Rudd21@users.noreply.github.com>
2023-01-03 17:15:23 +0200

    Add files via upload

---
+++ java/last.js
@@ -0,0 +1,59 @@
+function andrii() {
+    let info = "Пішла після 4 класу. Була відмицею, ходить в гімназію . Think about ХЗ.";
+    document.getElementById('result').innerHTML = info;
+}
+
+function fonarik() {
+    let info = "Пішов після 7 класу. Був ярік а просив називати фонарік . Think about математика, фізика.";
+    document.getElementById('result').innerHTML = info;
+}
+
+function vov4ik() {
+    let info = "Пішов після 7 класу. Був відмінником, високим і любив сказанути яку фігню на камеру, добре зна що таке smm враншиза :D. Think about біологія і хімія.";
+    document.getElementById('result').innerHTML = info;
+}
+
+function yavorina() {
+    let info = "Пішлов після 7 класу. З прізвища зрозуміло що він ще той мудрий дуб(тоб-то Яворський = Яворина). Think about все крім Хімії.";
+    document.getElementById('result').innerHTML = info;
+}
+
+function gre4ka() {
+    let info = "Пішла після 7 класу. Ви прочитавши всю інформацію маєте знати що ви зара дойшли до душнили бо поправляє слова, ходить в гімназію . Think about ХЗ.";
+    document.getElementById('result').innerHTML = info;
+}
+
+function feed() {
+    let info = "Пішлов після 4 класу. Інтересний тіп тим що він перейшов в наш клас в 4(на початку 1 семестру) і пішов зразуж після закінчення 4 XD, ходить в гімназію . Think about ХЗ.";
+    document.getElementById('result').innerHTML = info;
+}
+
+function hz() {
+    let info = "Пішла після 1 класу. Якщо судити по інстаграмі і встановити геолокацію місця де було зроблено це фото, то зрозумло що вона переїхала в Угорники . Think about ХЗ.";
+    document.getElementById('result').innerHTML = info;
+}
+
+function petrushka() {
+    let info = "Пішлjd після 7 класу. Реальний ВОР огірків саме це зафіксував(якшо ви думаєте що людину можна зняти а не зафіксувати, то знімати можна шлюху а не людину на камеру) на камеру Влад . Think about ХЗ.";
+    document.getElementById('result').innerHTML = info;
+}
+
+function mikola() {
+    let info = "Пішла після 4 класу. По розказав Яріка(Славік) став страним тіпом . Think about ХЗ.";
+    document.getElementById('result').innerHTML = info;
+}
+
+function zaChoku() {
+    let info = "Пішла після 7 класу. Всрата/Странна привичка мучити свої ночи сидячи на них, настояща мазахістка крч. Think about ХЗ";
+    document.getElementById('result').innerHTML = info;
+}
+
+function mudriiDub() {
+    let info = "Пішла після 6 класу. Пішла вона в іншу паралель, чому? Відповідь: по рофлу. Є і буде дубом щей лисіюшим. Think about ХЗ.";
+    document.getElementById('result').innerHTML = info;
+}
+
+function putin() {
+    let info = "Пішлов після 3 класу. Самий тормознутий тіп який був в класі, но скоріше всього він був тормознутий через те що час в нашому світі пришвидчувався а в нього ні . Think about ХЗ.";
+    document.getElementById('result').innerHTML = info;
+}
